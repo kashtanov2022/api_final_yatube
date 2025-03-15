6bbf2645f6476a5b70a2d605be4060cce6e65713
Invisi <kashtanov.2022.2022@gmail.com>
2025-03-15 16:30:08 +0700

    20

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -22,6 +22,7 @@ class CommentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Comment
         fields = '__all__'
+        read_only_fields = ('author', 'post')
 
 
 class FollowSerializer(serializers.ModelSerializer):
