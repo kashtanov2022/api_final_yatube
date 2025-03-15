de4b6da0e886d85bce1fbcccb64166b27867b5d3
Invisi <kashtanov.2022.2022@gmail.com>
2025-03-15 15:31:31 +0700

    9

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -12,7 +12,7 @@ router_v1 = routers.DefaultRouter()
 router_v1.register(r'posts', PostViewSet)
 router_v1.register(r'users', UserViewSet)
 router_v1.register(r'groups', GroupViewSet)
-router_v1.register(r'follow', FollowViewSet)
+router_v1.register(r'follow', FollowViewSet, basename='follow')
 router_v1.register(r'posts/(?P<post_id>\d+)/comments',
                    CommentViewSet, basename='comments')
 
