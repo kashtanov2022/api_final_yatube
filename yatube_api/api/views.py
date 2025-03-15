949c4e7c0b45f4daddebceee1ba5e687aa6e3267
Invisi <kashtanov.2022.2022@gmail.com>
2025-03-15 14:38:46 +0700

    2

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets, mixins, permissions, filters
-from rest_framework.response import Response
 
 from posts.models import Post, Comment, Follow
 from .serializers import PostSerializer, CommentSerializer, FollowSerializer
@@ -19,21 +18,27 @@ class FollowViewSet(mixins.CreateModelMixin,
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
-        
+
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrReadOnly
+    ]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
-        
+
 
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
+    permission_classes = [
+        permissions.IsAuthenticatedOrReadOnly,
+        IsAuthorOrReadOnly
+    ]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
--- yatube_api/posts/models.py
+++ yatube_api/posts/models.py
@@ -1,8 +1,8 @@
 from django.contrib.auth import get_user_model
 from django.db import models
 
-
 User = get_user_model()
+
 
 class Post(models.Model):
     text = models.TextField()
